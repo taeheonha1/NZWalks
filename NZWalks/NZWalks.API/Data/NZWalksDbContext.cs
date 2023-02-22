db96552c513e3e1403046758b0af9fb5768a4ee6
tha <taeheonha1@gmail.com>
2023-02-21 18:16:33 -0800

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        // Inject our dependency into the services
+        // Dependency injection is a software design pattern. It is a techniqe for achieving inversion of contorl
+        // between classes and their dependencies.
+        // In this pattern, the dependencies of a class are injected into them rather then the class directly
+        //Inject NZWalkDB into the service
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Population { get; set; }
+
+        //Naviation Property
+        //[1] Every Region is related to Walks.-- One Region can have multiple Walks.
+        public IEnumerable<Walk> Walks { get; set; }
+
+    }
+}
